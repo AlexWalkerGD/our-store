4b134a251520f28cddf01e99ce74bdccbbf8553a
AlexWalkerGD <alexwalkerson@hotmail.com>
2026-05-22 02:32:48 +0100

    feat: add first banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,16 @@ import Image from "next/image";
 
 export default function Home() {
   const { data } = useSession();
-  return <div>{data?.user?.name}</div>;
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        className="h-auto w-full"
+        sizes="100vw"
+        alt="Até 55% de desconto esse mês!"
+      />
+    </div>
+  );
 }
