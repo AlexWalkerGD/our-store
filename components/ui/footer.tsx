27cc29b7f2c949b46aca4639b06b181ac57f4ecd
AlexWalkerGD <alexwalkerson@hotmail.com>
2026-05-25 23:35:38 +0100

    feat: add Footer component

---
+++ components/ui/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-sm opacity-75">
+      © 2026 Copyright
+      <span className="font-semibold"> Our Store</span>
+    </div>
+  );
+};
+
+export default Footer;
