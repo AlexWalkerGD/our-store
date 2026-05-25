10889c70fdd355ceca8c5f2a0d023ca2f60e304c
AlexWalkerGD <alexwalkerson@hotmail.com>
2026-05-26 00:49:03 +0100

    feat: add CategoryItem component

---
+++ app/catalog/page.tsx
@@ -0,0 +1,27 @@
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/category-item";
+
+const CatalogPage = async () => {
+  const categories = await prismaClient.category.findMany({});
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="border-primary w-fit gap-2 border-2 px-3 py-4 text-base uppercase"
+        variant="outline"
+      >
+        <ShapesIcon size={16} />
+        Catálogo
+      </Badge>
+
+      <div className="grid grid-cols-2 flex-nowrap gap-8">
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CatalogPage;
