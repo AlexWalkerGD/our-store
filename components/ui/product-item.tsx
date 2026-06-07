83db1b5820b9728bba8463e436dad3360e7c8cb3
AlexWalkerGD <alexwalkerson@hotmail.com>
2026-06-07 16:58:53 +0100

    feat: add design product list to desktop

--- components/ui/product-item.tsx
+++ components/ui/product-item.tsx
@@ -1,54 +1,56 @@
 import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
-import { ArrowDownIcon } from "lucide-react";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
+  className?: string;
 }
 
-const ProductItem = ({ product }: ProductItemProps) => {
+const ProductItem = ({ product, className }: ProductItemProps) => {
   return (
-    <Link href={`/product/${product.slug}`}>
-      <div className="flex flex-col gap-4">
-        <div className="bg-accent relative flex h-[170px] w-full items-center justify-center rounded-lg">
-          <Image
-            src={product.imageUrls[0]}
-            height={0}
-            width={0}
-            sizes="100vw"
-            className="h[90px] max-h-[70%] w-auto max-w-[80%]"
-            style={{ objectFit: "contain" }}
-            alt={product.name}
-          />
-          {product.discountPercent > 0 && (
-            <Badge className="absolute top-3 left-2 px-2 py-[2px]">
-              <ArrowDownIcon size={14} /> {product.discountPercent}%
-            </Badge>
-          )}
-        </div>
+    <Link
+      href={`/product/${product.slug}`}
+      className={cn("flex min-w-[156px] flex-col gap-4", className)}
+    >
+      <div className="bg-accent relative flex aspect-square w-full items-center justify-center rounded-lg">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+          alt={product.name}
+        />
 
-        <div className="flex flex-col gap-1">
-          <p className="overflow-hidden text-sm text-ellipsis whitespace-nowrap">
-            {product.name}
-          </p>
-          <div className="flex items-center gap-2">
-            {product.discountPercent > 0 ? (
-              <>
-                <p className="font-semibold">
-                  € {product.totalPrice.toFixed(2)}
-                </p>
-                <p className="text-xs line-through opacity-75">
-                  € {Number(product.basePrice).toFixed(2)}
-                </p>
-              </>
-            ) : (
-              <p className="text-xs line-through opacity-75">
-                € {Number(product.basePrice).toFixed(2)}
+        {product.discountPercent > 0 && (
+          <Badge className="absolute top-3 left-3">
+            {product.discountPercent}
+          </Badge>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <p className="truncate text-sm">{product.name}</p>
+
+        <div className="flex items-center gap-2">
+          {product.discountPercent > 0 ? (
+            <>
+              <p className="truncate font-semibold lg:text-lg">
+                R$ {product.totalPrice.toFixed(2)}
               </p>
-            )}
-          </div>
+
+              <p className="truncate text-xs line-through opacity-75 lg:text-sm">
+                R$ {Number(product.basePrice).toFixed(2)}
+              </p>
+            </>
+          ) : (
+            <p className="truncate text-sm font-semibold">
+              R$ {product.basePrice.toFixed(2)}
+            </p>
+          )}
         </div>
       </div>
     </Link>
